8a9843b9cc25b6c60fca379dc1c152e398af423f
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-10 15:11:25 +0200

    added Ingredient, UnitOfMeasure entities

--- src/main/java/com/nadarzy/springrecipeapp/model/Notes.java
+++ src/main/java/com/nadarzy/springrecipeapp/model/Notes.java
@@ -7,8 +7,11 @@ public class Notes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @OneToOne
     private Recipe recipe;
+
+    @Lob
     private String recipeNotes;
 
     public Long getId() {
--- src/main/java/com/nadarzy/springrecipeapp/model/Recipe.java
+++ src/main/java/com/nadarzy/springrecipeapp/model/Recipe.java
@@ -1,103 +1,108 @@
 package com.nadarzy.springrecipeapp.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private String description;
-    private Integer prepTime;
-    private Integer cookTime;
-    private Integer servings;
-    private String source;
-    private String url;
-    private String directions;
-    private Byte[] image;
-    @OneToOne(cascade = CascadeType.ALL)
-    private Notes notes;
+  private String description;
+  private Integer prepTime;
+  private Integer cookTime;
+  private Integer servings;
+  private String source;
+  private String url;
+  private String directions;
 
+  @Lob private Byte[] image;
 
-    public Long getId() {
-        return id;
-    }
+  @OneToOne(cascade = CascadeType.ALL)
+  private Notes notes;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+  private Set<Ingredient> ingredients;
 
-    public String getDescription() {
-        return description;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public Integer getPrepTime() {
-        return prepTime;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setPrepTime(Integer prepTime) {
-        this.prepTime = prepTime;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public Integer getCookTime() {
-        return cookTime;
-    }
+  public Integer getPrepTime() {
+    return prepTime;
+  }
 
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
+  public void setPrepTime(Integer prepTime) {
+    this.prepTime = prepTime;
+  }
 
-    public Integer getServings() {
-        return servings;
-    }
+  public Integer getCookTime() {
+    return cookTime;
+  }
 
-    public void setServings(Integer servings) {
-        this.servings = servings;
-    }
+  public void setCookTime(Integer cookTime) {
+    this.cookTime = cookTime;
+  }
 
-    public String getSource() {
-        return source;
-    }
+  public Integer getServings() {
+    return servings;
+  }
 
-    public void setSource(String source) {
-        this.source = source;
-    }
+  public void setServings(Integer servings) {
+    this.servings = servings;
+  }
 
-    public String getUrl() {
-        return url;
-    }
+  public String getSource() {
+    return source;
+  }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+  public void setSource(String source) {
+    this.source = source;
+  }
 
-    public String getDirections() {
-        return directions;
-    }
+  public String getUrl() {
+    return url;
+  }
 
-    public void setDirections(String directions) {
-        this.directions = directions;
-    }
+  public void setUrl(String url) {
+    this.url = url;
+  }
 
-    public Byte[] getImage() {
-        return image;
-    }
+  public String getDirections() {
+    return directions;
+  }
 
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
+  public void setDirections(String directions) {
+    this.directions = directions;
+  }
 
-    public Notes getNotes() {
-        return notes;
-    }
+  public Byte[] getImage() {
+    return image;
+  }
 
-    public void setNotes(Notes notes) {
-        this.notes = notes;
-    }
+  public void setImage(Byte[] image) {
+    this.image = image;
+  }
+
+  public Notes getNotes() {
+    return notes;
+  }
+
+  public void setNotes(Notes notes) {
+    this.notes = notes;
+  }
 }
