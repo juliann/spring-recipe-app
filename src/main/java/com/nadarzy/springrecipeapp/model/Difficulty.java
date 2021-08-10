2ebf2d199fc31ba3d7a4f3b1b8f741b6a0a6880e
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-10 15:20:54 +0200

    added Difficulty Enum

---
+++ src/main/java/com/nadarzy/springrecipeapp/model/Difficulty.java
@@ -0,0 +1,7 @@
+package com.nadarzy.springrecipeapp.model;
+
+public enum Difficulty {
+  EASY,
+  MODERATE,
+  HARD;
+}
--- src/main/java/com/nadarzy/springrecipeapp/model/Recipe.java
+++ src/main/java/com/nadarzy/springrecipeapp/model/Recipe.java
@@ -20,6 +20,9 @@ public class Recipe {
 
   @Lob private Byte[] image;
 
+  @Enumerated(EnumType.STRING)
+  private Difficulty difficulty;
+
   @OneToOne(cascade = CascadeType.ALL)
   private Notes notes;
 
@@ -104,5 +107,21 @@ public class Recipe {
 
   public void setNotes(Notes notes) {
     this.notes = notes;
+  }
+
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
+
+  public void setDifficulty(Difficulty difficulty) {
+    this.difficulty = difficulty;
+  }
+
+  public Set<Ingredient> getIngredients() {
+    return ingredients;
+  }
+
+  public void setIngredients(Set<Ingredient> ingredients) {
+    this.ingredients = ingredients;
   }
 }
