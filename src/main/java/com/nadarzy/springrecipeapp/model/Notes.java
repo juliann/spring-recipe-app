ae7843355e6ddb013af5112862095300b1afa028
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-12 13:17:47 +0200

    added Project Lombok support; added @Data for model, added logging via @Slf4j

--- src/main/java/com/nadarzy/springrecipeapp/model/Notes.java
+++ src/main/java/com/nadarzy/springrecipeapp/model/Notes.java
@@ -1,40 +1,23 @@
 package com.nadarzy.springrecipeapp.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Notes {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @OneToOne
-    private Recipe recipe;
+  @OneToOne private Recipe recipe;
 
-    @Lob
-    private String recipeNotes;
+  @Lob private String recipeNotes;
 
-    public Long getId() {
-        return id;
-    }
+  public Notes() {}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
+  protected boolean canEqual(final Object other) {
+    return other instanceof Notes;
+  }
 }
--- src/main/java/com/nadarzy/springrecipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/nadarzy/springrecipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.nadarzy.springrecipeapp.services;
 
 import com.nadarzy.springrecipeapp.model.Recipe;
 import com.nadarzy.springrecipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
   @Override
   public Set<Recipe> getRecipes() {
+    log.debug("in the service");
     Set<Recipe> recipeSet = new HashSet<>();
     recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
     return recipeSet;
