5f45db9d1dce0043a37c67be1a41abb9d492057c
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-12 13:36:12 +0200

    minor fixed, added @Transactional

--- src/main/java/com/nadarzy/springrecipeapp/model/Category.java
+++ src/main/java/com/nadarzy/springrecipeapp/model/Category.java
@@ -1,11 +1,13 @@
 package com.nadarzy.springrecipeapp.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
   @Id
