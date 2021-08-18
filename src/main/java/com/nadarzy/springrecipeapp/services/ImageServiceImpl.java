ebe8b0bcd567bed00d3564d3d47d1029e6044359
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-18 11:47:17 +0200

    upload images to database working

--- src/main/java/com/nadarzy/springrecipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/nadarzy/springrecipeapp/services/ImageServiceImpl.java
@@ -1,10 +1,48 @@
 package com.nadarzy.springrecipeapp.services;
 
+import com.nadarzy.springrecipeapp.model.Recipe;
+import com.nadarzy.springrecipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+  private final RecipeRepository recipeRepository;
+
+  public ImageServiceImpl(RecipeRepository recipeService) {
+
+    this.recipeRepository = recipeService;
+  }
+
   @Override
-  public void saveImageFile(Long id, MultipartFile multipartFile) {}
+  @Transactional
+  public void saveImageFile(Long recipeId, MultipartFile file) {
+
+    try {
+      Recipe recipe = recipeRepository.findById(recipeId).get();
+
+      Byte[] byteObjects = new Byte[file.getBytes().length];
+
+      int i = 0;
+
+      for (byte b : file.getBytes()) {
+        byteObjects[i++] = b;
+      }
+
+      recipe.setImage(byteObjects);
+
+      recipeRepository.save(recipe);
+    } catch (IOException e) {
+      // todo improve exception handling
+      log.error("Error occurred", e);
+
+      e.printStackTrace();
+    }
+  }
 }
